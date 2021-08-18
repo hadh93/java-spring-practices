257e11e350fdbec58f2e6459b5895a630132039c
hadh93 <hoondog1993@gmail.com>
2021-08-18 23:53:46 +0900

    DB updated (+2 tables, item and order_detail) and entities defined accordingly. FIXME: test for new DB not functional.

---
+++ src/test/java/com/example/study/repository/ItemRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.Item;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ItemRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    public void create(){
+        Item item = new Item();
+        item.setName("노트북");
+        item.setPrice(100000);
+        item.setContent("삼성 노트북");
+
+        Item newItem = itemRepository.save(item);
+        Assertions.assertNotNull(newItem);
+    }
+
+    @Test
+    public void read(){
+
+    }
+
+    @Test
+    public void update(){
+
+    }
+
+    @Test
+    public void delete(){
+
+    }
+
+}
