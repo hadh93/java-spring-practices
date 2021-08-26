3c0d8244216c33d6ecc9f64b864cb0e2ba8f1f74
hadh93 <hoondog1993@gmail.com>
2021-08-26 23:46:08 +0900

    DB ERD newly defined & model revision started (Category model defined)

---
+++ src/main/java/com/example/study/model/entity/Category.java
@@ -0,0 +1,31 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Category {
+    @Id
+    private Long id;
+
+    private String type;
+
+    private String title;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
+}
